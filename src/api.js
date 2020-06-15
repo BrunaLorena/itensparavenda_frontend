ab775a919e799ab3833190f8bf7844192fbbb135
BrunaLorena <brunalfs2011@gmail.com>
2020-06-15 23:09:04 +0000

    ok

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'https://itensparavenda-backend.herokuapp.com/'
+    baseURL:'// https://itensparavenda.herokuapp.com/itens'
 })
 
 export default api;
